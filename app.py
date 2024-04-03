ff7b94338cc582d42ba425c026b8f0bf487f84ad
Sayantan Podder <102387195+sayantan-02@users.noreply.github.com>
2024-04-03 22:03:01 +0000

    1st Update

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from app import create_app
+from flask import json
+from flask import request
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__': 
-    app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/github', methods=['POST'])
+def api_gh_messages():
+    if request.headers['Content-Type'] == 'application/json':
+        data = json.dumps(request.json)
+        print(data)
+        return data
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=80,debug=True)
+
+
+
